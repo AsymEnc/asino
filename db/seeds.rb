75368a216d776fe23eebb7308eaa8d9fe25679ed
Andrei Beliankou <arbox@yandex.ru>
2014-10-21 23:40:48 +0200

    Test articles as seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,3 @@
 require "#{Rails.root}/db/seeds/test_user.rb"
-require "#{Rails.root}/db/seeds/articles.rb"
+#require "#{Rails.root}/db/seeds/articles.rb"
+require "#{Rails.root}/db/seeds/test_article.rb"
