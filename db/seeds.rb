ec93b643958bba9caafe1ae72ead9486e90f93a7
Andrei Beliankou <arbox@yandex.ru>
2015-02-15 17:10:49 +0100

    Reworked seeding infrastructure.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 require "#{Rails.root}/db/seeds/test_user.rb"
-#require "#{Rails.root}/db/seeds/articles.rb"
 require "#{Rails.root}/db/seeds/de_collection.rb"
 require "#{Rails.root}/db/seeds/nl_collection.rb"
+require "#{Rails.root}/db/seeds/be_collection.rb"
---
+++ db/seeds/nl_collection.rb
@@ -0,0 +1,39 @@
+require 'csv'
+require 'date'
+
+FILES_TO_PARSE = %w(hermione_1.csv hermione_2.csv hermione_3.csv)
+
+FILES_TO_PARSE.map! { |f| "#{Rails.root}/db/seeds/#{f}" }
+
+params = {
+  :col_sep => "\t",
+  :row_sep => "\r\n",
+  :quote_char => '"',
+  :headers => true
+}
+
+threads = []
+m = Mutex.new
+
+FILES_TO_PARSE.each do |file|
+  threads << Thread.new do
+    CSV.foreach(file, params) do |row|
+      m.synchronize do
+        generated_date = Date.parse(row['paper_dc_date']).to_datetime.to_s
+        Document.create!(
+                         {
+                           :docid => row['_id'],
+                           :collection => row['paper_dc_language'],
+                           :fulltext => row['text_content'],
+                           :date => generated_date,
+                           :title => row['article_dc_title'],
+                           :location => row['paper_dcterms_spatial_creation'],
+                           :image_url => row['article_dc_identifier_resolver']
+                         }
+                         )
+      end
+    end
+  end
+end
+
+threads.each(&:join)
