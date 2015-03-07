dd20aae6acd11d53e434147b743d97e4d6006339
Andrei Beliankou <arbox@yandex.ru>
2015-03-07 19:07:00 +0100

    Changed column type for text.

--- db/migrate/20140624134000_create_documents.rb
+++ db/migrate/20140624134000_create_documents.rb
@@ -3,7 +3,7 @@ class CreateDocuments < ActiveRecord::Migration
     create_table :documents do |t|
       t.string :docid
       t.string :title
-      t.string :fulltext
+      t.text   :fulltext
       t.date   :date
       t.string :location
       t.string :collection
