6982124590f51e9bd9786e886d11d42c0f155480
Andrei Beliankou <arbox@yandex.ru>
2015-03-07 20:18:47 +0100

    Changes after user creation.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150307183706) do
+ActiveRecord::Schema.define(version: 20150307185721) do
 
   create_table "documents", force: :cascade do |t|
     t.string   "docid"
